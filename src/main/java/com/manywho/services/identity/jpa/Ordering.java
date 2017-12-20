3b85fde290c7801d915ed1526baa3f57f504e428
Jonjo McKay <jonjo.mckay@manywho.com>
2017-12-20 12:25:12 +0000

    Updated to SDK 2.0.0-beta-01

--- src/main/java/com/manywho/services/identity/jpa/Ordering.java
+++ src/main/java/com/manywho/services/identity/jpa/Ordering.java
@@ -1,14 +1,15 @@
 package com.manywho.services.identity.jpa;
 
+import com.manywho.sdk.api.run.elements.type.ListFilter;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.ComparableExpressionBase;
 
 public class Ordering {
-    public static OrderSpecifier createOrderSpecifier(String orderDirection, ComparableExpressionBase path) {
-        switch (orderDirection.toUpperCase()) {
-            case "ASC":
+    public static OrderSpecifier createOrderSpecifier(ListFilter.OrderByDirectionType orderDirection, ComparableExpressionBase path) {
+        switch (orderDirection) {
+            case Ascending:
                 return path.asc();
-            case "DESC":
+            case Descending:
             default:
                 return path.desc();
         }
